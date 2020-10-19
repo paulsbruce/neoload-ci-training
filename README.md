cec7acde6ece4a8fcfd17a7953d0ea165466bbbb
Paul Bruce <me@paulsbruce.io>
2020-10-19 15:39:31 -0400

    Update README and module5 pipeline

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# neoload-ci-training
+
+# NeoLoad in Jenkins Workshop
+
+This repo contains examples used during instructor-led training/workshop for running performance tests with the NeoLoad Platform in Jenkins pipelines.
+
+If you would like to participate in a hands-on workshop, please contact your Neotys representative to learn more.
+
+## Modules
+1. [Running your First Jenkins test from the examples](modules/module1)
+2. [Conceptual Architecture: What’s Going on Here and Why This is Scalable](modules/module2)
+3. [Basic Go/no-go Signals: exit codes, test summary, SLA results](modules/module3)
+4. [Dynamic artifacts: on-the-fly YAML, execution parameters, environment variables](modules/module4)
+5. [Parallel testing considerations: SuT sanity, cleanup, and asynchronous flows](modules/module5)
+6. Git branches, tagging, and promoting changes to test assets
+7. Infrastructure provisioning models & capacity planning
+
+## Additional Resources
+- [NeoLoad in Jenkins Training Deck](https://docs.google.com/presentation/d/13TD3HNlzlIOzjmeGsNnyF__QEZdshwa_3nEvccwIas4/edit?usp=sharing)
+- [NeoLoad as-code Training Deck](https://docs.google.com/presentation/d/19ldKFuET6ost-dkq_aPiIw0SIOf8ltFcKAUYS1iAVMk/edit?usp=sharing)
---
+++ infra/jenkins.sh
@@ -0,0 +1,40 @@
+docker ps -q --filter "name=jenkins-docker" | grep -q . && docker stop jenkins-docker
+docker ps -q --filter "name=jenkins-blueocean" | grep -q . && docker stop jenkins-blueocean
+
+NLW_HOST=nlweb.shared
+NLW_HOST_IP=$(host $NLW_HOST | head -n1 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
+echo $NLW_HOST_IP
+
+INT_HOST_IP=$(ifconfig | grep '10.0.' | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | head -n1)
+echo $INT_HOST_IP
+
+docker container run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind
+
+docker container run \
+  --name jenkins-blueocean \
+  --rm \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --add-host nlweb.shared:$NLW_HOST_IP \
+  --add-host gitbucket:$INT_HOST_IP \
+  jenkinsci/blueocean
+
+docker exec -it --user root jenkins-blueocean apk add -q --no-progress --upgrade bind-tools
