bec3f37693168b27cdc9e28d57c6ffbcaddca00e
Paul Bruce <me@paulsbruce.io>
2020-10-23 19:35:15 -0400

    Pull hosts logic into separate file

---
+++ infra/local_jenkins/hosts.add.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ ! ping -c1 host.docker.internal &>/dev/null ]; then
+  echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\thost.docker.internal" | tee -a /etc/hosts > /dev/null
+fi
+
+if [ ! ping -c1 host.docker.internal &>/dev/null ]; then
+  echo "Could not resolve host.docker.internal from inside the Docker host"
+  exit 1
+fi
+
+echo 'Adding gitbucket to hosts'
+if [ ! ping -c1 gitbucket &>/dev/null ]; then
+  GITBUCKET_IP=$(ping -c 1 -t 1 host.docker.internal | head -n1 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
+  echo -e "$GITBUCKET_IP\tgitbucket" | tee -a /etc/hosts > /dev/null
+fi
+
+cat /etc/hosts
