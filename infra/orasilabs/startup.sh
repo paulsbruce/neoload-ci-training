8f3e31a73acd4786535bad899e1e789f6fd8270c
Paul Bruce <me@paulsbruce.io>
2020-10-28 09:01:18 -0400

    Add orasilabs startup script

--- infra/orasilabs/startup.sh
+++ infra/orasilabs/startup.sh
@@ -3,5 +3,9 @@ set -e
 sudo apt-get install git
 HOME=/home/orasilabs
 mkdir -p $HOME/startup/
-cd $HOME/startup/ && git clone https://github.com/paulsbruce/neoload-ci-training.git
+if [ ! -d "$HOME/startup/neoload-ci-training" ]; then
+  cd $HOME/startup/ && git clone https://github.com/paulsbruce/neoload-ci-training.git
+else
+  cd $HOME/startup/neoload-ci-training && git pull
+fi
 $HOME/startup/neoload-ci-training/infra/local_jenkins/start.sh
