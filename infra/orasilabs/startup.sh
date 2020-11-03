c6dcc50f007fa4576bae2c4320bf13290f00cc19
Paul Bruce <me@paulsbruce.io>
2020-11-03 16:06:26 -0500

    Add a pause at end of orasi startup script

--- infra/orasilabs/startup.sh
+++ infra/orasilabs/startup.sh
@@ -26,25 +26,6 @@ fi
 
 cd $HOME_DIR/startup/neoload-ci-training && git checkout $git_branch
 
+export HOME_DIR=$HOME_DIR
 
-JENKINS_HTTP_PORT=80
-NLW_HOST=nlweb.shared
-NLW_HOST_API_BASE=http://$NLW_HOST:8080
-
-NLW_HOST_IP=$(sudo ping -c1 -t1 -W1 $NLW_HOST 2>/dev/null | head -n1 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
-if [ -z "$NLW_HOST_IP" ]; then
-  echo "Cannot resolve the IP address for $NLW_HOST. Please discuss with your workshop instructor."
-  exit 3
-else
-  sleep 3
-fi
-
-export JENKINS_HTTP_PORT=$JENKINS_HTTP_PORT
-export NLW_HOST=$NLW_HOST
-export NLW_HOST_IP=$NLW_HOST_IP
-# every time this VM is booted, run the initial Jenkins setup (persists data between sessions)
-cd $HOME_DIR/startup/neoload-ci-training && infra/local_jenkins/start.sh $@
-echo "----------------------------------------------------------"
-echo "--  COPY THE JENKINS TOKEN ABOVE!!!"
-echo "----------------------------------------------------------"
-read -p "Once you copy this token, press enter to close this window"
+cd $HOME_DIR/startup/neoload-ci-training && infra/orasilabs/start_after_git.sh $@
