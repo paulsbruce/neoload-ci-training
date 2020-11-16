d1033b9019822f8659c2cd953e1dcd924a0c411b
Paul Bruce <me@paulsbruce.io>
2020-11-16 13:10:19 -0500

    Update wait for keypress

--- infra/orasilabs/start_after_git.sh
+++ infra/orasilabs/start_after_git.sh
@@ -15,7 +15,4 @@ export NLW_HOST=$NLW_HOST
 export NLW_HOST_IP=$NLW_HOST_IP
 # every time this VM is booted, run the initial Jenkins setup (persists data between sessions)
 cd $HOME_DIR/startup/neoload-ci-training && infra/local_jenkins/start.sh $@
-echo "----------------------------------------------------------"
-echo "--  COPY THE JENKINS TOKEN ABOVE!!!"
-echo "----------------------------------------------------------"
-read -n1 -s -p "Once you copy this token, press enter to close this window"
+cd $HOME_DIR/startup/neoload-ci-training && infra/local_jenkins/startup_after_pause.sh
---
+++ infra/orasilabs/startup_after_pause.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "----------------------------------------------------------"
+echo "--  COPY THE JENKINS TOKEN ABOVE!!!"
+echo "----------------------------------------------------------"
+read -p "Once you copy this token, press enter to close this window"
